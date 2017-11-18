7fea3fb42d45d039c53596c389efbc3cd0b4e206
Yv Heng <kongyh-wa15@student.tarc.edu.my>
2017-11-18 19:07:27 +0800

    Full UI 2

--- app/src/main/java/info/androidhive/fingerprint/FingerprintActivity.java
+++ app/src/main/java/info/androidhive/fingerprint/FingerprintActivity.java
@@ -57,8 +57,7 @@ public class FingerprintActivity extends AppCompatActivity {
              * startActivity(intent);
              */
             textView.setText("Your Device does not have a Fingerprint Sensor");
-        }else
-            {
+        }else{
             // Checks whether fingerprint permission is set on manifest
             if (ActivityCompat.checkSelfPermission(this, Manifest.permission.USE_FINGERPRINT) != PackageManager.PERMISSION_GRANTED) {
                 textView.setText("Fingerprint authentication permission not enabled");
